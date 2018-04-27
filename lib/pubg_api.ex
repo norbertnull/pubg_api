315af5f0ad6eb68f4123515cc876d29c8cd64846
Norberts Kakste <norberts@mesh.lv>
2018-04-28 02:00:18 +0300

    Initial commit

---
+++ lib/pubg_api.ex
@@ -0,0 +1,18 @@
+defmodule PubgApi do
+  @moduledoc """
+  Documentation for PubgApi.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PubgApi.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
