abf21cc01d948b3ba912da600dd937884a3a34c3
Norberts Kakste <norberts@mesh.lv>
2018-04-28 04:34:48 +0300

    Delete default generated code.

--- lib/pubg_api.ex
+++ lib/pubg_api.ex
@@ -1,18 +1,2 @@
 defmodule PubgApi do
-  @moduledoc """
-  Documentation for PubgApi.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> PubgApi.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
