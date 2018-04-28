7178d527899bd2bfc52b662a340c681ea6e76326
Norberts Kakste <norberts@mesh.lv>
2018-04-28 03:47:43 +0300

    API Key module

---
+++ lib/pubg_api/api_key.ex
@@ -0,0 +1,10 @@
+defmodule PubgApi.ApiKey do
+    def generate(api_key) when is_bitstring(api_key) do
+        "Bearer " <> api_key
+    end
+
+    def add_to_headers(header_map, api_key) when is_bitstring(api_key) and is_map(header_map) do
+        header_map
+        |> Map.put("Authorization", PubgApi.ApiKey.generate(api_key))
+    end
+end
